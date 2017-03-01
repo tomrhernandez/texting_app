8d421f98ff9caaa32bd19fde8772fec7f857ca6c
tomrhernandez <tomrhernandez@gmail.com>
2017-03-01 17:20:08 +0000

    Table improvements, added totals for read and unread messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Store.create(name: "First Pharmacy", phone: "14043419132", nabp: "1111111")
-Store.create(name: "Second Pharmacy", phone: "14043419117", nabp: "2222222")
+#Store.create(name: "First Pharmacy", phone: "14043419132", nabp: "1111111")
+#Store.create(name: "Second Pharmacy", phone: "14043419117", nabp: "2222222")
 
 ### Simulated messages to and from Patient and Pharmacy ###
 
@@ -20,11 +20,17 @@ Message.create(to: "14043419132", from: "16168813870", message: "Do I have any r
 Message.create(to: "16168813870", from: "14043419132", message: "You have one refill left, would you like to get it filled?", store_id: 1, qs_read: true)
 Message.create(to: "14043419132", from: "16168813870", message: "Need refills for mine and my wife's prescriptions, thank you", store_id: 1)
 Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 1, qs_read: true)
+Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 1, qs_read: true)
+Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 2, qs_read: true)
+Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 2, qs_read: true)
+Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 3, qs_read: true)
+Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 3, qs_read: true)
+Message.create(to: "16168813870", from: "14043419132", message: "All your medications are ready. Thank you and have a nice day!", store_id: 3, qs_read: true)
 
-for message in Message.all
-    message.update(store_id: 1)
-    if message.id % 2 == 0
-        message.update(qs_read: true)
-    end
-    message.save
-end
+#for message in Message.all
+#    message.update(store_id: 1)
+#    if message.id % 2 == 0
+#        message.update(qs_read: true)
+#    end
+#    message.save
+#end
